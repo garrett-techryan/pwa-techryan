d66fa738c8646e2270a51676d06e06f0429e13a9
Garrett Chandler <chandlergarrett2@gmail.com>
2025-05-08 17:00:49 -0400

    Fixed redirect URI

--- src/authConfig.js
+++ src/authConfig.js
@@ -2,7 +2,7 @@ export const msalConfig = {
     auth: {
       clientId: "14d267d0-19e0-4103-8cfa-1a7e9f0b4cc1", // from Azure portal
       authority: "https://login.microsoftonline.com/d72ae6fa-03a7-4ccb-888c-64613b1d665b",
-      redirectUri: "http://localhost:3000",
+      redirectUri: "https://pwa.techryan.net",
     },
     cache: {
       cacheLocation: "localStorage",
